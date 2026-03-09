457799c0a0e205fb098a9629365499570603e768
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-09 18:00:23 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -1,28 +1,20 @@
 import json
 import os
 
-from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.dir import dirr
 from VIPMUSIC.core.git import git
-from VIPMUSIC.core.userbot import Userbot
 from VIPMUSIC.platforms.youtube import vipboy
 from VIPMUSIC.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
 dirr()
-
 git()
-
 dbb()
-
 heroku()
-
 sudo()
 
-app = VIPBot()
-
-userbot = Userbot()
+vipboy()
 
 from VIPMUSIC.platforms import (
     YouTubeAPI,
@@ -41,4 +33,5 @@ Apple = AppleAPI()
 Resso = RessoAPI()
 SoundCloud = SoundAPI()
 Telegram = TeleAPI()
+
 HELPABLE = {}
