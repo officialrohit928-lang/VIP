a7bed4491e7f794e1f15fab004a576e1a5a61ffc
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-09 18:16:50 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -1,37 +1,3 @@
-import json
-import os
-
-from VIPMUSIC.core.dir import dirr
-from VIPMUSIC.core.git import git
-from VIPMUSIC.platforms.youtube import vipboy
-from VIPMUSIC.misc import dbb, heroku, sudo
-
 from .logging import LOGGER
-
-dirr()
-git()
-dbb()
-heroku()
-sudo()
-
-vipboy()
-
-from VIPMUSIC.platforms import (
-    YouTubeAPI,
-    CarbonAPI,
-    SpotifyAPI,
-    AppleAPI,
-    RessoAPI,
-    SoundAPI,
-    TeleAPI
-)
-
-YouTube = YouTubeAPI()
-Carbon = CarbonAPI()
-Spotify = SpotifyAPI()
-Apple = AppleAPI()
-Resso = RessoAPI()
-SoundCloud = SoundAPI()
-Telegram = TeleAPI()
 
 HELPABLE = {}
