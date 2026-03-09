e348871cd7ff573fa4166ce3accfb8f7eec4ebc6
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-09 18:35:39 +0530

    Update app.py

--- VIPMUSIC/core/app.py
+++ VIPMUSIC/core/app.py
@@ -1,6 +1,8 @@
 import asyncio
 
-asyncio.set_event_loop(asyncio.new_event_loop())
+# Create and set event loop
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 
 from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.userbot import Userbot
